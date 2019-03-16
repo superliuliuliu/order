c0de35ae01c2686f16688aec8ec0a5f15dc3ad7f
superliu <734190426@qq.com>
2019-03-16 23:33:55 +0800

    商品基础api

---
+++ order/src/main/java/com/lgy/order/controller/BuyerProductController.java
@@ -0,0 +1,9 @@
+package com.lgy.order.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/buyer/product")
+public class BuyerProductController {
+}
