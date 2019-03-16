66445adb468b502327bd1a31915b6cdca6c9fd8a
superliu <734190426@qq.com>
2019-03-16 23:32:26 +0800

    商品基础dao层编写

---
+++ order/src/main/java/com/lgy/order/repository/ProductInfoRepository.java
@@ -0,0 +1,12 @@
+package com.lgy.order.repository;
+
+import com.lgy.order.dataobject.ProductInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductInfoRepository extends JpaRepository<ProductInfo,String> {
+
+    //根据商品的状态查询商品信息
+    List<ProductInfo> findByProductStatus(Integer productStatus);
+}
