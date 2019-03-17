3496f8b4602462efd993bbb0ffb4fe1083527aaf
superliu <734190426@qq.com>
2019-03-17 16:30:50 +0800

    新增订单主类 对应数据库中的订单主表

---
+++ order/src/main/java/com/lgy/order/dataobject/OrderMaster.java
@@ -0,0 +1,63 @@
+package com.lgy.order.dataobject;
+
+import com.lgy.order.enums.OrderStatusEnum;
+import com.lgy.order.enums.PayStatusEnum;
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@DynamicUpdate
+@Data
+@Entity
+@Table(name = "order_master")
+public class OrderMaster {
+
+    @Id
+    @Size(max = 32)
+    @Column(name = "order_id")
+    private String orderId;
+
+    @Column(name = "buyer_name")
+    private String buyerName;
+
+    @Column(name = "buyer_phone")
+    private String buyerPhone;
+
+    @Column(name = "buyer_address")
+    private String buyerAddress;
+
+    @Column(name = "buyer_openid")
+    private String buyerOpenid;
+
+    @Column(name = "order_amount")
+    private BigDecimal orderAmount;
+
+    /**订单状态 初始化为新订单状态*/
+    @Column(name = "order_status")
+    private Integer orderStatus = OrderStatusEnum.NEW.getCode();
+
+    /**支付状态 初始化为未支付状态*/
+    @Column(name = "pay_status")
+    private Integer payStatus = PayStatusEnum.WAIT.getCode();
+
+    /**
+     * 创建时间
+     */
+    @Column(name = "create_time")
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    @Column(name = "update_time")
+    private Date updateTime;
+
+
+}
