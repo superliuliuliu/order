7972bcbae8ab646d4c27a1b297d6a9956ce3a4fd
superliu <734190426@qq.com>
2019-03-17 23:48:07 +0800

    订单信息异常类  用来处理产生订单时 出现的各种错误

---
+++ order/src/main/java/com/lgy/order/exception/SellException.java
@@ -0,0 +1,15 @@
+package com.lgy.order.exception;
+
+import com.lgy.order.enums.ResultEnum;
+
+public class SellException extends RuntimeException{
+
+    private Integer code;
+
+    public SellException(ResultEnum resultEnum){
+        //因为RuntimeException含有message这个属性
+        super(resultEnum.getMessage());
+        this.code = resultEnum.getCode();
+    }
+
+}
