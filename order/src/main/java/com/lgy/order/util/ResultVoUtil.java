5a5ad24ab3f44f684777221a08fc96ca0beeae4e
superliu <734190426@qq.com>
2019-03-17 16:32:21 +0800

    新增util工具类  用来说明API获取数据的状态

---
+++ order/src/main/java/com/lgy/order/util/ResultVoUtil.java
@@ -0,0 +1,38 @@
+package com.lgy.order.util;
+
+import com.lgy.order.VO.ResultVo;
+
+
+/**
+ * ResultVoUtil
+ * @description 返回结果工具类
+ * @param
+ * @return
+ * @author liugaoyang
+ * @date 2019/3/17 10:49
+ * @version 1.0.0
+ */
+public class ResultVoUtil {
+
+    public static ResultVo success(Object object){
+        ResultVo resultVo = new ResultVo();
+        resultVo.setCode(0);
+        resultVo.setMessage("成功");
+        resultVo.setData(object);
+        return resultVo;
+    }
+
+    //成功但是无数据返回的情况
+    public static ResultVo success(){
+        return success(null);
+    }
+
+    //响应失败的情况
+    public static ResultVo error(Integer code, String message){
+        ResultVo resultVo = new ResultVo();
+        resultVo.setCode(code);
+        resultVo.setMessage(message);
+        return  resultVo;
+    }
+
+}
