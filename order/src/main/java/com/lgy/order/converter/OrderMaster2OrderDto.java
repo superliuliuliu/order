21f924dc73a6652797d9744f1840eb45dcfd3014
superliu <734190426@qq.com>
2019-03-20 18:06:27 +0800

    类型转换工具类  将orderMaster转为orderDto

---
+++ order/src/main/java/com/lgy/order/converter/OrderMaster2OrderDto.java
@@ -0,0 +1,37 @@
+package com.lgy.order.converter;
+
+import com.lgy.order.dataobject.OrderMaster;
+import com.lgy.order.dto.OrderDto;
+import org.springframework.beans.BeanUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * OrderMaster2OrderDto
+ * @description 类型转换器将OrderMaster类转换为OrderDto
+ * @param
+ * @return
+ * @author liugaoyang
+ * @date 2019/3/19 21:55
+ * @version 1.0.0
+ */
+
+public class OrderMaster2OrderDto {
+
+    private OrderMaster2OrderDto() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static OrderDto convert(OrderMaster orderMaster){
+        OrderDto orderDto = new OrderDto();
+        BeanUtils.copyProperties(orderMaster, orderDto);
+        return orderDto;
+    }
+
+    //
+    public static List<OrderDto> convert(List<OrderMaster> orderMasterList){
+        return orderMasterList.stream().map(e-> convert(e)).collect(Collectors.toList());
+    }
+
+}
