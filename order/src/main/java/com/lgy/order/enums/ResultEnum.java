1ecd49bb2c2115784c05d37eeb7639f2680da9e2
刘高阳 <liugaoyang@thunisoft.com>
2019-03-22 16:28:36 +0800

    更新错误枚举类

--- order/src/main/java/com/lgy/order/enums/ResultEnum.java
+++ order/src/main/java/com/lgy/order/enums/ResultEnum.java
@@ -16,18 +16,33 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResultEnum {
 
+    PARAM_ERROR(1, "参数异常"),
+
     PRODUCT_NOT_EXIST(10, "商品不存在"),
+
     PRODUCT_STOCK_ERROR(11, "商品库存错误"),
+
     ORDER_NOT_EXIST(12, "订单不存在"),
+
     ORDER_DETAIL_NOT_EXIST(13, "订单详情不存在"),
+
     ORDER_STATE_ERROR(14, "订单状态不正确"),
+
     ORDER_UPDATE_ERROR(15, "订单更新失败"),
+
     ORDER_DETAIL_NULL(16, "订单详情为空"),
+
     ORDER_FINISH_ERROR(17, "订单完结失败"),
+
     ORDER_ALREADY_FINISH(18, "订单重复完结"),
+
     ORDER_ALREADY_PAY(19, "订单重复支付"),
+
     ORDER_PAY_ERROR(20, "订单支付失败"),
-    ORDER_TIMEOUT(21, "订单已过期")
+
+    ORDER_TIMEOUT(21, "订单已过期"),
+
+    CART_EMPTY(22, "购物车中无商品"),
     ;
 
     private Integer code;
