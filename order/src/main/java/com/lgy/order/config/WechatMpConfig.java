cc56ea3c0a1b85e5884732b90b97f93abfd8f0a1
superliu <734190426@qq.com>
2019-03-25 09:44:27 +0800

    配置类 为wxMpService配置appid appsecret等属性   方便后续调用

---
+++ order/src/main/java/com/lgy/order/config/WechatMpConfig.java
@@ -0,0 +1,38 @@
+package com.lgy.order.config;
+
+import me.chanjar.weixin.mp.api.WxMpConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * WechatMpConfig
+ * @description 微信Mpservice配置类
+ * @author liugaoyang
+ * @date 2019/3/24 18:24
+ * @version 1.0.0
+ */
+@Component
+public class WechatMpConfig {
+
+    @Autowired
+    private WechatAccountConfig accountConfig;
+
+    @Bean
+    public WxMpService wxMpService(){
+        WxMpService wxMpService = new WxMpServiceImpl();
+        wxMpService.setWxMpConfigStorage(wxMpConfigStorage());
+        return wxMpService;
+    }
+
+    @Bean
+    public WxMpConfigStorage wxMpConfigStorage(){
+        WxMpInMemoryConfigStorage wxMpInMemoryConfigStorage = new WxMpInMemoryConfigStorage();
+        wxMpInMemoryConfigStorage.setAppId(accountConfig.getMpAppId());
+        wxMpInMemoryConfigStorage.setSecret(accountConfig.getMpAppSecret());
+        return wxMpInMemoryConfigStorage;
+    }
+}
