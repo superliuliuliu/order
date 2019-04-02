dfdc611e92ee7f72642f7c401c55bfe55880d046
superliuliuliu <734190426@qq.com>
2019-04-02 14:52:03 +0800

    移除无用的import

--- order/src/main/java/com/lgy/order/service/Impl/OrderServiceImpl.java
+++ order/src/main/java/com/lgy/order/service/Impl/OrderServiceImpl.java
@@ -26,7 +26,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
