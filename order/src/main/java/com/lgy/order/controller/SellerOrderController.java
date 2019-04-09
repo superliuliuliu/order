3352a7f949e7fa37c7fc23e6cf5d0ae7f0e5f7b4
superliuliuliu <734190426@qq.com>
2019-04-09 16:57:50 +0800

    最新代码

--- order/src/main/java/com/lgy/order/controller/SellerOrderController.java
+++ order/src/main/java/com/lgy/order/controller/SellerOrderController.java
@@ -11,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
