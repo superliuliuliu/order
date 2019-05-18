1433b60deeaa5670ed71f5720c3487ffb1be1048
superliu <734190426@qq.com>
2019-05-18 15:23:35 +0800

    包结构调整

---
+++ order/src/main/java/com/lgy/order/mapper/ProductCategoryMapper.java
@@ -0,0 +1,12 @@
+package com.lgy.order.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+
+import java.util.Map;
+
+
+public interface ProductCategoryMapper {
+
+    @Insert("insert into product_category (category_name, category_type) values(#{category_name, jdbcType=VARCHAR}, #{category_type, jdbcType=INTEGER})")
+    int insertByMap(Map<String, Object> map);
+}
--- order/src/test/java/com/lgy/order/OrderApplicationTests.java
+++ order/src/test/java/com/lgy/order/OrderApplicationTests.java
@@ -1,16 +1,33 @@
 package com.lgy.order;
 
+import com.lgy.order.common.util.KeyUtil;
+import com.lgy.order.mapper.ProductCategoryMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OrderApplicationTests {
 
+	@Autowired
+	private ProductCategoryMapper productCategoryMapper;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testMybatis(){
+		Map<String, Object> map = new HashMap<>();
+		map.put("category_name", "麻辣专区");
+		map.put("category_type", 4);
+		productCategoryMapper.insertByMap(map);
 	}
 
 }
